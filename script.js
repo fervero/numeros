14798a11f14f598c9f4ac5eaa7ce5015933fbd5a
Maciej Bojko <maciej.bojko@gmail.com>
2017-01-19 18:34:31 +0100

    Made a little more object-oriented, functional, and manageable.

--- script.js
+++ script.js
@@ -1,27 +1,155 @@
 var speaker,
-    mois = ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
     lastMessage = "",
-    failureMessage,
-    successMessage,
+    failureUtterance,
+    successUtterance,
     currentInput,
     inputShort,
     inputDate,
     inputPhone,
+    lastUtterance,
+    $submitButton,
     $answersHere,
-    verify = function() {return};
+    $shortButton,
+    $dateButton,
+    $phoneButton,
+    verify = function() {return false};
 
-function sayFrench(phrase) {
-    var msg = new SpeechSynthesisUtterance(phrase);
-    msg.lang = "fr-FR";
-    speaker.speak(msg);
+var frenchController = {
+    failureMessage: "C'est incorrect.",
+    successMessage: "C'est correct.",
+    startMessage: "Salut !",
+    lang: "fr-FR",
+    months: ["janvier", "février", "mars", "avril", "mai", "juin", "juillet", "août", "septembre", "octobre", "novembre", "décembre"],
+
+    utterance: function(phrase) {
+        var msg = new SpeechSynthesisUtterance(phrase);
+        msg.lang = this.lang;
+        return msg;
+    },
+    
+    sayHello: function() {
+        this.bindButtons();
+        successUtterance = this.utterance(this.successMessage);
+        failureUtterance = this.utterance(this.failureMessage);
+        speaker.speak(this.utterance(this.startMessage));
+    },    
+    
+    randomShort: function() {
+        return Math.round(1000 * Math.random());
+    },  
+        
+    formatShort: function(x) {
+        var stringified = String(x);
+        return {
+            pattern: stringified,
+            placeholder: stringified.replace(/./g,"#")
+        }
+    },
+            
+    verifyShort: function (answer, pattern) {
+        return answer == pattern;
+    },
+    
+    randomPhone: function() {
+        return this.formatPhone(Math.round(100000000 + 899999999 * Math.random()));
+    },
+    
+    formatPhone: function(x) {
+        return x.toString().match(/.{1,2}/g).join("-");
+    },    
+    
+    verifyPhone: function(answer, pattern) {
+        var sliceAnswer = parseInt(answer.split("-").join(""));
+        return this.formatPhone(sliceAnswer) == pattern;        
+    },
+
+    randomDate: function(start, end) {
+        var day = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        return this.formatDate(day.getDate(), day.getMonth(), day.getFullYear());
+    },
+    
+    formatDate: function(day, month, year) {
+        return day + " " + this.months[parseInt(month)] + " " + year;        
+    },
+      
+    verifyDate: function(answer, pattern) {
+        var sliceAnswer = answer.split("-");
+        return this.formatDate(sliceAnswer [0], parseInt(sliceAnswer [1]) - 1, sliceAnswer [2]) == pattern;
+    },
+    
+    tryGame: function (randomGenerator, verifyFunction, inputWindow) {
+        speaker.cancel();
+        lastMessage = randomGenerator;
+        speaker.speak(lastUtterance = this.utterance(lastMessage) );
+        substituteCurrent(inputWindow);
+        $submitButton.off("click")
+            .click(this.verifyAnswer.bind(this, verifyFunction) );                
+        inputDate.hide();
+        inputPhone.hide();
+        inputShort.hide();
+        inputWindow.show();
+    },
+
+    tryShort: function() {
+        this.tryGame.call(
+            this,
+            this.randomShort(),
+            this.verifyShort,
+            inputShort
+        );
+    },
+    
+    tryDate: function() {
+        this.tryGame.call(
+            this, 
+            this.randomDate(new Date(1969, 0, 1), new Date()), 
+            this.verifyDate, 
+            inputDate);
+    },
+    
+    tryPhone: function() {
+        this.tryGame.call(
+            this,
+            this.randomPhone(),
+            this.verifyPhone,
+            inputPhone
+        );
+    },
+    
+    verifyAnswer: function(verifyFunction) {
+        verifyFunction = verifyFunction || verify;
+        var $inputField = $answersHere.find("input:visible"),
+            answer = $inputField.val();
+        if (verifyFunction.call(this, answer, lastMessage)) 
+            saySuccess();
+        else 
+            sayFailure();
+    },
+    
+    bindButtons: function() {
+        $shortButton.off("click")
+            .click(this.tryShort.bind(this) );
+        $phoneButton.off("click")
+            .click(this.tryPhone.bind(this) );
+        $dateButton.off("click")
+            .click(this.tryDate.bind(this) );        
+    }
 }
 
+var spanishController = Object.create(frenchController);
+
+spanishController.months = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+spanishController.startMessage = "Hola!";
+spanishController.failureMessage = "Es incorrecte.";
+spanishController.successMessage = "Es correcte.";
+spanishController.lang = "es-ES";
+
 function saySuccess() {
-    speaker.speak(successMessage);
+    speaker.speak(successUtterance);
 }
 
 function sayFailure() {
-    speaker.speak(failureMesage);
+    speaker.speak(failureUtterance);
 }
 
 function substituteCurrent(newInput) {
@@ -33,88 +161,9 @@ function substituteCurrent(newInput) {
     currentInput = newInput;
 }
 
-function randomNumber() {
-    return Math.round(1000 * Math.random());
-}
-
-function tryShort() {
-    speaker.cancel();
-    sayFrench(lastMessage = randomNumber());
-    verify = verifyShort;
-    this.blur();
-    substituteCurrent(inputShort);
-    inputDate.hide();
-    inputPhone.hide();
-    inputShort.show();
-}
-
-function verifyShort (answer, pattern) {
-    return answer == pattern;
-}
-
-function formatDate(day, month, year) {
-    return day + " " + mois[parseInt(month)] + " " + year;
-}
-
-function randomDate(start, end) {
-    var day = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    return formatDate(day.getDate(), day.getMonth(), day.getFullYear());
-}
-
-function tryDate() {
-    speaker.cancel();
-    sayFrench (lastMessage = randomDate(new Date(1969, 0, 1), new Date()) );
-    verify = verifyDate;
-    this.blur();
-    substituteCurrent(inputDate);
-    inputPhone.hide();
-    inputShort.hide();
-    inputDate.show();
-}
-
-function verifyDate (answer, pattern) {
-    var sliceAnswer = answer.split("-");
-    return formatDate(sliceAnswer [0], parseInt(sliceAnswer [1]) - 1, sliceAnswer [2]) == pattern;
-}
-
-function formatPhone(x) {
-    return x.toString().match(/.{1,2}/g).join("-");
-}
-
-function randomPhone() {
-    return formatPhone(Math.round(100000000 + 899999999 * Math.random()));
-}
-
-function tryPhone() {
-    speaker.cancel();
-    sayFrench(lastMessage = randomPhone());
-    this.blur();
-    verify = verifyPhone;
-    substituteCurrent(inputPhone);
-    inputShort.hide();
-    inputDate.hide();    
-    inputPhone.show();
-}
-
-function verifyPhone(answer, pattern) {
-    var sliceAnswer = parseInt(answer.split("-").join(""));
-    return formatPhone(sliceAnswer) == pattern;
-}
-
 function repeat() {
     speaker.cancel();
-    sayFrench(lastMessage);
-    this.blur();
-}
-
-function checkAnswer() {
-    var $inputField = $answersHere.find("input:visible"),
-        answer = $inputField.val();
-    if (verify(answer, lastMessage)) 
-        saySuccess();
-    else 
-        sayFailure();
-    this.blur();
+    speaker.speak(lastUtterance);
 }
 
 function noVoices() {
@@ -127,7 +176,6 @@ function findFrenchVoice(list) {
     }
     return false;
 }
-
 
 function init() {
     if(!("speechSynthesis" in window)) {
@@ -145,24 +193,25 @@ function init() {
     }
 }
 
-
 function secondInit() {
-    sayFrench("Salut!");
+    $submitButton = $("#button-submit");
+    frenchController.sayHello();
     inputShort = $("#answer-short");
     inputDate = $("#answer-date");
     inputPhone = $("#answer-phone");
     $answersHere = $("#answers-here");
-    successMessage = new SpeechSynthesisUtterance("Correct.");
-    successMessage.lang = "fr-FR";
-    failureMesage = new SpeechSynthesisUtterance("Incorrect.");
-    failureMesage.lang = "fr-FR";
+    $shortButton = $("#button-short");
+    $dateButton = $("#button-date");
+    $phoneButton = $("#button-phone");
     $("h2").css("visibility", "hidden");
     $("#button-repeat").click(repeat);
-    $("#button-submit").click(checkAnswer);
-    $("#button-short").removeAttr("disabled").click(tryShort);    
-    $("#button-date").removeAttr("disabled").click(tryDate);
-    $("#button-phone").removeAttr("disabled").click(tryPhone);
-
+    $shortButton
+        .removeAttr("disabled");
+    $dateButton
+        .removeAttr("disabled");
+    $phoneButton
+        .removeAttr("disabled");
+    frenchController.bindButtons();
 }
 
 function thirdInit() {
